145c6833bf98bcea2219c389a38e6d2ad340fa7e
kli1992 <463643865@qq.com>
2023-03-22 18:17:32 +0800

    随便提交一点东西

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -1,8 +1,11 @@
-import javax.swing.*;
+import java.util.Optional;
 
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("叫大哥");
         System.out.println("哈哈哈哈");
+        int count = 0;
+        String s = "s";
+        Optional.ofNullable(s);
     }
 }
