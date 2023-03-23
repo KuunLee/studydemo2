4a4dd03cd1c98e061b6b0a73c1ff6d3cd3f4d6d4
kli1992 <463643865@qq.com>
2023-03-23 08:51:21 +0800

    随便提交一点东西

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -7,5 +7,15 @@ public class Demo1 {
         int count = 0;
         String s = "s";
         Optional.ofNullable(s);
+        int a = 0;
+        int b = 0;
+        for (int i = 10; i >= 0; i--) {
+            a = b + i;
+            b++;
+            if(a == i - b){
+                break;
+            }
+        }
+        System.out.printf("i");
     }
 }
